07b60bb43caf087e4b4123d47e681ec787e287a5
KaelDucatti <ducatti860@gmail.com>
2025-07-31 22:22:04 -0300

    configuring DRF

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-iisq1aa*$7p9)l^s=40+ohn9#syf)1m6a1jb(odqg46lces)i9"
+SECRET_KEY = (
+    "django-insecure-iisq1aa*$7p9)l^s=40+ohn9#syf)1m6a1jb(odqg46lces)i9"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "genres",
 ]
 
