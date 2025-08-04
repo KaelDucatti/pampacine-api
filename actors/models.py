81031f95ad66b2d75364419584d8fccd18f9b04a
KaelDucatti <ducatti860@gmail.com>
2025-08-04 12:59:12 -0300

    updating Genre model and creating Actor model

--- actors/models.py
+++ actors/models.py
@@ -1,9 +1,35 @@
 from django.db import models
 
 
+class Nationality(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    acronym = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"{self.name} ({self.acronym})"
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Nationalities"
+
+
 class Actor(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    gender = models.CharField(max_length=1)
+    birthday = models.DateField(null=True, blank=True)
+    nationality = models.ForeignKey(
+        Nationality,
+        on_delete=models.PROTECT,
+        related_name="actors_nationality",
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+        verbose_name_plural = "Actors"
--- genres/models.py
+++ genres/models.py
@@ -4,8 +4,16 @@ from django.db import models
 class Genre(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
+        return (
+            f"{self.name} (active)"
+            if self.active
+            else f"{self.name} (inactive)"
+        )
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Genres"
