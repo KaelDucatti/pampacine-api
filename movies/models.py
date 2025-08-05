f9730cdbcb25fab7ee81e94155f37980b4281e18
KaelDucatti <ducatti860@gmail.com>
2025-08-05 17:58:30 -0300

    craeting models & serializers for movies app

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from actors.models import Actor
+from genres.models import Genre
+
+
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=500)
+    release_date = models.DateField(null=True, blank=True)
+    movie_cast = models.ManyToManyField(Actor, related_name="movie-cast")
+    resume = models.TextField(null=True, blank=True)
+    genres = models.ForeignKey(
+        Genre, on_delete=models.PROTECT, related_name="movie-genres"
+    )
