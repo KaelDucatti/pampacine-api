dbc90a2df117de871aac7e5c396b37dca1bdb327
KaelDucatti <ducatti860@gmail.com>
2025-08-09 23:31:19 -0300

    adding validators to 'MovieCreateUpdateDestroySerializer()'

--- movies/serializers.py
+++ movies/serializers.py
@@ -32,3 +32,17 @@ class MovieCreateUpdateDestroySerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError(
+                "A data de lançamento não pode ser inferior a 1900."
+            )
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError(
+                "O resumo não pode ultrapassar 200 caracteres."
+            )
+        return value
