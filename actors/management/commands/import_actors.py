04fc9db1ac9b44499510a6ab7bd6012e394526b5
KaelDucatti <ducatti860@gmail.com>
2025-08-28 16:57:20 -0300

    craete automation to insert actors by csv file

--- actors/management/commands/import_actors.py
+++ actors/management/commands/import_actors.py
@@ -1,4 +1,9 @@
+import csv
+from datetime import datetime
+
 from django.core.management.base import BaseCommand
+
+from actors.models import Actor
 
 
 class Command(BaseCommand):
@@ -8,3 +13,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         file_name = options["file_name"]
         print(f"file name: {file_name}")
+
+        with open("actors.csv", "r", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                first_name = row["first_name"]
+                last_name = row["last_name"]
+                birthday = datetime.strptime(
+                    row["birthday"], "%Y-%m-%d"
+                ).date()
+                nationality = row["nationality"]
+
+                Actor.objects.create(
+                    first_name=first_name,
+                    last_name=last_name,
+                    birthday=birthday,
+                    nationality=nationality,
+                )
+        self.stdout.write(self.style.SUCCESS("Atores importados com sucesso!"))
